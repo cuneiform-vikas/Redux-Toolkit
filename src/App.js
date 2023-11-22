d705b8622dac39004443ddb1e684a1bde63354d0
Vikas <vikas.parmar@thecuneiform.com>
2023-11-22 11:53:53 +0530

    Installed Redux library with RTK: Created store, provided to index.js and dispatched actions in App.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  selectCount,
+} from "./redux/counterSlice";
 
-function App() {
+const App = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Redux CreateAPI Demo</h1>
+
+      <h1>{count}</h1>
+
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>+1</button>
+      <button onClick={() => dispatch(incrementByAmount(3))}>+3</button>
     </div>
   );
-}
+};
 
 export default App;
